c88c4203ec14440be46a56560ed84d1017aa76c8
Walterhart <walterwhart1@gmail.com>
2023-12-04 16:11:05 -0500

    Complete challenge

--- App.jsx
+++ App.jsx
@@ -12,6 +12,7 @@ export default function App() {
     const [score, setScore] = React.useState(STARTING_SCORE)
     const [playSong] = useSound("../audio/song.mp3")
     const [playClick] = useSound("../audio/click.mp3", {volume: .45})
+
     
 /* Challenge 
 
@@ -19,25 +20,56 @@ export default function App() {
 
         1. When the user clicks on the start button... 
             - the timer starts counting down seconds.
-            - the class of "fade-in" is replaced with "fade-out" in the start button's classList.
+            - the class of "fade-in" is replaced with "fade-out" in the start ("play") button's    
+              classList.
             - the start button is disabled. 
             - the song and click sound get played by invoking playSong() and playClick(). 
         
-        2. At 0 seconds, the timer stops, and "fade-out" gets replaced with "fade-in" in the start 
-           button's classList. 
+        2. At 0 seconds, the timer stops, and "fade-out" gets replaced with "fade-in" in 
+           the start button's classList. 
         
         3. If the player then clicks the start button again, the timer gets reset to 60 seconds and 
            the score gets reset to 0, and everything listed in task 1 happens again. 
         
         4. To accomplish these tasks, you *only* need to write code below these comments; you 
             *don't* need to change or add anything above them or in a different file! 
+            
+        1) start game
+            - on click start game timer count down in second. When game start playSong()
+            - When clicked on object playClick(). 
 */
+    React.useEffect(()=>{
+        let interval
+        
+        if(timerRunning && timeLeft > 0){ 
+            interval = setInterval(()=>{
+            setTimeLeft(prevTime=>prevTime - 1)
+        }, 1000) 
+        }else if(timeLeft === 0){
+            setTimerRunning(false)
+        }
+       
+       return () => {
+        clearInterval(interval)
+       }
+    },[timerRunning])
+    
+    const handleStartGame = () =>{
+        if(!timerRunning){
+            setTimeLeft(STARTING_TIME)
+            setScore(STARTING_SCORE)
+        }
+        setTimerRunning(true)
+        playSong()
+
+    }
+ 
     
     return (
         <div>
             <ScoreBoard data={{score, timeLeft}}/>
-            <PlayArea playProps={{timeLeft, timerRunning, setScore}}/>
-            <button className="play-button fade-in">Play</button>
+            <PlayArea playProps={{timeLeft, timerRunning, setScore}} />
+            <button className={`play-button ${timerRunning ? "fade-out":"fade-in"}`} onClick={handleStartGame} disabled={timerRunning}>Play</button>
         </div>
         )
     }
